1b88f857c0e9e8d78ec32a02fb897613951a8041
Julien Fontanet <julien.fontanet@isonoe.net>
2017-10-02 16:08:44 +0200

    feat(pages): todos implementation

--- pages/src/App.js
+++ pages/src/App.js
@@ -1,21 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Map } from 'immutable'
+
+import logo from './logo.svg'
+import TodoList from './TodoList.js'
+import './App.css'
 
 class App extends Component {
+  state = {
+    todos: new Map()
+  }
+
+  _onTodosChange = todos => {
+    this.setState({ todos })
+  }
+
   render() {
+    const { state } = this
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">My Todos</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        {/* those two lists will share the same data */}
+        <TodoList
+          onChange={this._onTodosChange}
+          todos={state.todos}
+        />
+        <TodoList
+          onChange={this._onTodosChange}
+          todos={state.todos}
+        />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ pages/src/TodoList.js
@@ -0,0 +1,124 @@
+import ImmutablePropTypes from 'react-immutable-proptypes'
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import { createSelector } from 'reselect'
+
+export default class TodoList extends Component {
+  static propTypes = {
+    onChange: PropTypes.func.isRequired,
+    todos:ImmutablePropTypes.mapOf(
+      PropTypes.shape({
+        completed: PropTypes.bool.isRequired,
+        label: PropTypes.string.isRequired,
+        id: PropTypes.string.isRequired
+      }).isRequired,
+      PropTypes.string.isRequired
+    )
+  }
+
+  state = {
+    display: 'all',
+    newTodoLabel: ''
+  }
+
+  _createTodo = event => {
+    event.preventDefault()
+
+    const newTodoLabel = this.state.newTodoLabel.trim()
+    if (newTodoLabel === '') {
+      return
+    }
+
+    this.setState({ newTodoLabel: '' })
+
+    // all items of collection should have a unique identifier
+    const id = Math.random().toString(36).slice(2)
+
+    const { props } = this
+    props.onChange(props.todos.set(id, {
+      completed: false,
+      id,
+      label: newTodoLabel
+    }))
+  }
+
+  // - what's a selector?
+  // - what's a memoized selector?
+  //
+  // see https://github.com/reactjs/reselect
+  _getVisibleTodos = createSelector(
+    state => state.display,
+    (_, props) => props.todos,
+    (display, todos) => {
+      if (display === 'all') {
+        return todos.valueSeq()
+      }
+
+      return todos.valueSeq().filter(
+        display === 'active'
+          ? t => !t.completed
+          : t => t.completed
+      )
+    }
+  )
+
+  _onDisplayChange = ({ target }) => {
+    this.setState({ display: target.value })
+  }
+
+  _onNewTodoLabelChange = ({ target }) => {
+    this.setState({ newTodoLabel: target.value })
+  }
+
+  _onTodoCompletionChange = ({ target }) => {
+    const { id } = target.dataset
+    const { props } = this
+    const { todos } = props
+    props.onChange(todos.set(id, {
+      ...todos.get(id),
+      completed: target.checked
+    }))
+  }
+
+  render() {
+    const { state } = this
+    return (
+      <div>
+        <form onSubmit={this._createTodo}>
+          <p>
+            <input
+              onChange={this._onNewTodoLabelChange}
+              placeholder='Type and press <Enter> to create an item'
+              size='40'
+              value={state.newTodoLabel}
+            />
+          </p>
+        </form>
+        <select onChange={this._onDisplayChange} value={state.display}>
+          <option value='all'>All</option>
+          <option value='active'>Active</option>
+          <option value='completed'>Completed</option>
+        </select>
+        <ul>
+          {this._getVisibleTodos(state, this.props).map(({ completed, id, label }) =>
+            <li key={id}>
+              <label>
+                <input
+                  checked={completed}
+                  data-id={id}
+                  onChange={this._onTodoCompletionChange}
+                  type='checkbox'
+                />
+                {' '}
+                {completed
+                  ? <del>{label}</del>
+                  : label
+                }
+              </label>
+            </li>
+          )}
+        </ul>
+      </div>
+    )
+  }
+}
