864364da238b4279d4b82f11aec86d3afb77b635
Hamadoz <ghemid_m_dz@yahoo.fr>
2017-10-10 14:37:28 +0200

    server work

--- pages/src/App.js
+++ pages/src/App.js
@@ -1,80 +1,62 @@
 import React, { Component } from 'react'
 import { Map } from 'immutable'
-
+import { provideState, update, mergeIntoState } from "freactal";
 import logo from './logo.svg'
 import TodoList from './TodoList.js'
 import './App.css'
+import "isomorphic-fetch";
 
-class App extends Component {
-  state = {
-    todos: new Map()
-  }
-
-  componentWillMount () {
-    fetch('//localhost:20431/todos').then(res =>
-      res.json().then(todos => {
-        this.setState({
+const wrapComponentWithState = provideState({
+  initialState: () => ({
+    todos: new Map(),
+    newTodoLabel: '',
+    gitInfo: ''
+  }),
+  effects: {
+    initialize: (effects) => fetch('//localhost:20431/todos')
+      .then(res => res.json()
+        .then(todos => mergeIntoState({
           todos: new Map().withMutations(map => {
             todos.forEach(todo => {
               map.set(todo.id, todo)
             })
           })
         })
-      })
-    )
-  }
-
-  _onCreateTodo = todo => {
-    const { id } = todo
-    const { todos } = this.state
-    const previous = todos.get(id)
-
-    this.setState({ todos: todos.set(id, todo) })
-
-    fetch(`//localhost:20431/todos/${id}`, {
+        )),
+    sendToServer: (effects, todo, id) => fetch(`http://localhost:20431/todos/${id}`, {
       body: JSON.stringify(todo),
       headers: {
         'content-type': 'application/json'
       },
       method: 'PUT'
     }).catch(() => {
-      this.setState({
-        todos: previous === undefined
-          ? todos.delete(id)
-          : todos.set(id, previous)
-      })
-    })
+      console.log('caught promise')
+    }),
+    onCreateTodo: (effects) => state => {
+      const id = Math.random().toString(36).slice(2)
+      const todo = {
+        id,
+        label: state.newTodoLabel
+      }
+      effects.sendToServer(todo, id)
+      effects.createTodo(todo)
+      effects.freeInput()
+    },
+    freeInput: update(state => ({ newTodoLabel: '' })),
+    createTodo: update((state, todo) => ({ todos: state.todos.set(todo.id, todo) })),
+    setNewTodoLabelChange: update((state, val) => ({ newTodoLabel: val })),
   }
+})
 
-  _onEditTodo = this._onCreateTodo
+const App = wrapComponentWithState(({ state, effects }) => (
 
-  _onTodosChange = todos => {
-    this.setState({ todos })
-  }
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header>
+    <TodoList />
+  </div>
+))
 
-  render() {
-    const { state } = this
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">My Todos</h1>
-        </header>
-
-        {/* those two lists will share the same data */}
-        <TodoList
-          onCreateTodo={this._onCreateTodo}
-          onEditTodo={this._onEditTodo}
-          todos={state.todos}
-        />
-        <TodoList
-          onCreateTodo={this._onCreateTodo}
-          onEditTodo={this._onEditTodo}
-          todos={state.todos}
-        />
-      </div>
-    )
-  }
-}
-
-export default App
+export default App;
--- pages/src/TodoList.js
+++ pages/src/TodoList.js
@@ -2,123 +2,38 @@ import ImmutablePropTypes from 'react-immutable-proptypes'
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import { createSelector } from 'reselect'
+import { injectState } from "freactal";
+import { Map } from 'immutable'
 
-export default class TodoList extends Component {
-  static propTypes = {
-    onCreateTodo: PropTypes.func.isRequired,
-    onEditTodo: PropTypes.func.isRequired,
-    todos:ImmutablePropTypes.mapOf(
-      PropTypes.shape({
-        completed: PropTypes.bool.isRequired,
-        label: PropTypes.string.isRequired,
-        id: PropTypes.string.isRequired
-      }).isRequired,
-      PropTypes.string.isRequired
-    )
-  }
-
-  state = {
-    display: 'all',
-    newTodoLabel: ''
-  }
-
-  _createTodo = event => {
-    event.preventDefault()
-
-    const newTodoLabel = this.state.newTodoLabel.trim()
-    if (newTodoLabel === '') {
-      return
-    }
-
-    this.setState({ newTodoLabel: '' })
-
-    // all items of collection should have a unique identifier
-    const id = Math.random().toString(36).slice(2)
-    const todo = {
-      completed: false,
-      id,
-      label: newTodoLabel
-    }
-
-    this.props.onCreateTodo(todo)
-  }
-
-  // - what's a selector?
-  // - what's a memoized selector?
-  //
-  // see https://github.com/reactjs/reselect
-  _getVisibleTodos = createSelector(
-    state => state.display,
-    (_, props) => props.todos,
-    (display, todos) => {
-      if (display === 'all') {
-        return todos.valueSeq()
-      }
-
-      return todos.valueSeq().filter(
-        display === 'active'
-          ? t => !t.completed
-          : t => t.completed
+const TodoList = injectState(({ state, effects }) => {
+  
+      const onNewTodoLabelChange = ev => effects.setNewTodoLabelChange(ev.target.value)
+      const onCreateTodo = ev => { ev.preventDefault()
+                                   effects.onCreateTodo() }
+  
+      return (
+          <div>
+              <form onSubmit={onCreateTodo}>
+                  <p>
+                      <input
+                          onChange={onNewTodoLabelChange}
+                          placeholder='Type and press <Enter> to create an item'
+                          size='40'
+                          value={state.newTodoLabel}
+                      />
+                  </p>
+              </form>
+              <ul>
+                  {state.todos.valueSeq().map(({ id, label }) =>
+                      <li key={id}>
+                          <label>
+                              {label}
+                          </label>
+                      </li>
+                  )}
+              </ul>
+          </div>
       )
-    }
-  )
-
-  _onDisplayChange = ({ target }) => {
-    this.setState({ display: target.value })
-  }
-
-  _onNewTodoLabelChange = ({ target }) => {
-    this.setState({ newTodoLabel: target.value })
-  }
-
-  _onTodoCompletionChange = ({ target }) => {
-    const { id } = target.dataset
-    const { props } = this
-    props.onEditTodo({
-      ...props.todos.get(id),
-      completed: target.checked
-    })
-  }
-
-  render() {
-    const { state } = this
-    return (
-      <div>
-        <form onSubmit={this._createTodo}>
-          <p>
-            <input
-              onChange={this._onNewTodoLabelChange}
-              placeholder='Type and press <Enter> to create an item'
-              size='40'
-              value={state.newTodoLabel}
-            />
-          </p>
-        </form>
-        <select onChange={this._onDisplayChange} value={state.display}>
-          <option value='all'>All</option>
-          <option value='active'>Active</option>
-          <option value='completed'>Completed</option>
-        </select>
-        <ul>
-          {this._getVisibleTodos(state, this.props).map(({ completed, id, label }) =>
-            <li key={id}>
-              <label>
-                <input
-                  checked={completed}
-                  data-id={id}
-                  onChange={this._onTodoCompletionChange}
-                  type='checkbox'
-                />
-                {' '}
-                {completed
-                  ? <del>{label}</del>
-                  : label
-                }
-              </label>
-            </li>
-          )}
-        </ul>
-      </div>
-    )
-  }
-}
+  })
+  
+  export default TodoList
